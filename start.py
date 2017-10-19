bfef1298dc842978285cf4e16c764fb78127b448
Rose-LY <17743535393@163.com>
2017-10-19 19:54:29 +0800

    add log anderror dir,start file

---
+++ start.py
@@ -0,0 +1,30 @@
+#coding= utf-8
+
+import HTMLTestRunner as htr
+import unittest
+import sys,os
+from execute_friend import TestFriendChat
+
+os.system("rm -f %s/errorpng/*" %os.getcwd())
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    try:
+        webim2 = {
+                TestFriendChat:["testSignin","testLogin","testLogout","testaddFriend1","testrefuseFriend","testaddFriend2","testagreeFriend",
+                                "testsendImage","testsendFile","testMultiMess","testcleanchat","testaddblack","testremoveblack","testdelfriend"],
+
+                }
+        for classes,methlist in webim2.items():
+            for methods in methlist:
+                suite.addTest(classes(methods))
+
+    except ValueError,e:
+        print "your methodname is not exist: %s" % e
+
+    filename = '%s/logs/index.html' % os.getcwd()
+    fp = file(filename, 'wb')
+    runner = htr.HTMLTestRunner(
+        stream=fp,
+        title='Easemob Test Report',
+        description='WebIM2.0 Test Report')
+    runner.run(suite)
