a0e7d401df903e1ccfeada09f12c549ec8f3fba1
Rose-LY <17743535393@163.com>
2017-11-03 14:42:04 +0800

    add chatroom test function

---
+++ chat_room.py
@@ -0,0 +1,103 @@
+#coding: utf-8
+
+import selenium,os,sys
+import time
+from config import *
+from chat_friend import AloneChat
+from chat_group import ChatGroup
+sys.path.append(os.getcwd())
+
+class ChatRoom(ChatGroup):
+    def __init__(self,driver,user,passwd,url,friend,groupname):
+        super(ChatRoom, self).__init__(driver, user, passwd, url, friend,groupname)
+
+    def JoinChatroom(self):
+        funname = sys._getframe().f_code.co_name
+        try:
+            self.driver.find_element_by_xpath(
+                "//ul[@class='ant-menu ant-menu-horizontal x-header-tab__menu ant-menu-light ant-menu-root']/li[3]").click()
+            time.sleep(2)
+            self.driver.find_element_by_xpath("//div[@class='nav-text']/div").click()
+            try:
+                if self.driver.find_element_by_xpath("//div[@class='x-list-item x-chat-header']/div"):
+                    roomnumID = self.driver.find_element_by_xpath("//div[@class='x-list-item x-chat-header']/div").text
+                    print "join chat room success, chatroom id is: ",roomnumID
+                    return True,roomnumID
+                else:
+                    self.screenshot("%s_%s.png" % (funname, time.strftime('%H_%M_%S')))
+                    print "join chat room failed"
+                    return False,None
+            except Exception,error:
+                self.screenshot("%s_%s.png" % (funname, time.strftime('%H_%M_%S')))
+                print "join chat room failed",error
+                return False, None
+        except Exception,error:
+            self.screenshot("%s_%s.png" % (funname, time.strftime('%H_%M_%S')))
+            print "join chat room failed", error
+            return False, None
+
+
+    def JoinAssignRoom(self,roomnumID):
+        funname = sys._getframe().f_code.co_name
+        try:
+            time.sleep(2)
+            self.driver.find_element_by_xpath(
+                "//ul[@class='ant-menu ant-menu-horizontal x-header-tab__menu ant-menu-light ant-menu-root']/li[3]").click()
+            time.sleep(1)
+            self.driver.find_element_by_xpath("//div[@class='nav-text']/div").click()
+            nownum = self.driver.find_element_by_xpath("//div[@class='x-list-item x-chat-header']/div").text
+            for i in range(1, 8):
+                if str(nownum) != roomnumID:
+                    self.driver.find_element_by_xpath(
+                        "//ul[@class='ant-menu ant-menu-inline ant-menu-light ant-menu-root']/li[%d]" % i).click()
+                    nownum = self.driver.find_element_by_xpath("//div[@class='x-list-item x-chat-header']/div").text
+                    if i == 7:
+                        print "%s is not found" % roomnumID
+                        exit(100)
+                else:
+                    print "Join chatroom %s success" % roomnumID
+                    break
+            return True
+        except Exception,err:
+            self.screenshot("%s_%s.png" % (funname, time.strftime('%H_%M_%S')))
+            print "join chatroom %s failed" % roomnumID, err
+            return False
+    def sendroomMess(self,groupmess_num):
+        funname = sys._getframe().f_code.co_name
+        try:
+            self.driver.find_element_by_xpath(
+                "//ul[@class='ant-menu ant-menu-horizontal x-header-tab__menu ant-menu-light ant-menu-root']/li[3]").click()
+            time.sleep(2)
+            self.driver.find_element_by_xpath("//div[@class='nav-text']/div").click()
+            time.sleep(1)
+            self.sendimage()
+            self.sendfile()
+            for num in range(groupmess_num-2):
+                self.sendMSfirend()
+            return True
+        except Exception, error:
+            self.screenshot("%s_%s.png" % (funname, time.strftime('%H_%M_%S')))
+            print u"%s Failed" % funname, error
+            return False
+
+    def RoomMessNum(self,groupmess_num):
+        funname = sys._getframe().f_code.co_name
+        try:
+            time.sleep(2)
+            self.driver.find_element_by_xpath(
+                "//ul[@class='ant-menu ant-menu-horizontal x-header-tab__menu ant-menu-light ant-menu-root']/li[3]").click()
+            time.sleep(2)
+            self.driver.find_element_by_xpath("//div[@class='nav-text']/div").click()
+            try:
+                time.sleep(2)
+                if self.driver.find_element_by_xpath("//div[@class='x-chat-content']/div[%d]" %groupmess_num):
+                    print u"Verify chatroom message OK, number is: ", groupmess_num
+                    return True
+            except Exception, error:
+                self.screenshot("%s_%s.png" % (funname, time.strftime('%H_%M_%S')))
+                print u"Verify chatroom message failed, number is not %d" %groupmess_num, error
+                return False
+        except Exception, error:
+            self.screenshot("%s_%s.png" % (funname, time.strftime('%H_%M_%S')))
+            print u"%s Failed" % funname, error
+            return False
---
+++ execute_room.py
@@ -0,0 +1,74 @@
+#coding: utf-8
+
+from selenium import webdriver
+import time,sys,requests
+from config import *
+from chat_room import ChatRoom
+import unittest
+sys.path.append("..")
+
+class TestChatRoom(unittest.TestCase):
+    def setUp(self):
+        self.driver3 = webdriver.Chrome()
+        self.threeuser = ChatRoom(self.driver3, user3, gpasswd, url, user1,groupname)
+        options = webdriver.ChromeOptions()
+        options.add_argument('disable-infobars')
+        self.browser = webdriver.Chrome(chrome_options=options)
+        self.twouser = ChatRoom(self.browser, user2, gpasswd, url, user1,groupname)
+        driver1 = webdriver.Chrome()
+        self.oneuser = ChatRoom(driver1, user1, gpasswd, url, user2,groupname)
+
+    def testJoinChatroom_1(self):
+        u'验证加入聊天室'
+        self.oneuser.sign_in()
+        self.twouser.sign_in()
+        self.threeuser.sign_in()
+        self.oneuser.login()
+        self.twouser.login()
+        global roomID
+        result, roomID = self.oneuser.JoinChatroom()
+        self.twouser.JoinAssignRoom(roomID)
+        self.threeuser.login()
+        self.assertTrue(self.threeuser.JoinAssignRoom(roomID), True)
+    def testSendImage_2(self):
+        u'验证chatroom发送图片'
+        self.oneuser.login()
+        self.twouser.login()
+        self.oneuser.JoinAssignRoom(roomID)
+        self.twouser.JoinAssignRoom(roomID)
+        self.oneuser.sendimage()
+        self.assertTrue(self.twouser.receiveimage(), True)
+    def testSendFile_3(self):
+        u'验证chatroom发送文件'
+        self.oneuser.login()
+        self.twouser.login()
+        self.oneuser.JoinAssignRoom(roomID)
+        self.twouser.JoinAssignRoom(roomID)
+        self.oneuser.sendfile()
+        self.assertTrue(self.twouser.receivefile(), True)
+    def testSdMultiMess_4(self):
+        u'验证发送多条消息'
+        self.twouser.login()
+        self.threeuser.login()
+        self.twouser.JoinAssignRoom(roomID)
+        self.threeuser.JoinAssignRoom(roomID)
+        self.twouser.sendroomMess(groupmess_num)
+        self.assertTrue(self.threeuser.RoomMessNum(groupmess_num), True)
+    def testCleanMessage_5(self):
+        u'验证清除所有消息'
+        self.oneuser.login()
+        self.threeuser.login()
+        self.oneuser.JoinAssignRoom(roomID)
+        self.threeuser.JoinAssignRoom(roomID)
+        self.oneuser.sendroomMess(groupmess_num)
+        self.threeuser.RoomMessNum(groupmess_num)
+        self.threeuser.cleanchat()
+        self.assertFalse(self.threeuser.RoomMessNum(groupmess_num), False)
+
+    def tearDown(self):
+        self.oneuser.quitBrowser()
+        self.threeuser.quitBrowser()
+        self.twouser.quitBrowser()
+        self.oneuser = None
+        self.twouser = None
+        self.threeuser = None
